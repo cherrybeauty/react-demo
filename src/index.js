b269bb7e328026b2ca7a40d508f8652b03a3eafc
cherry_beauty <756474171@qq.com>
2022-08-14 15:46:08 +0800

    core

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,7 +13,75 @@ root.render(
 
   </React.StrictMode>
 );
+class Toggle extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      isToggleOn:true
+    };
+  }
+  handleClick(){
+    this.setState(prevState=>({
+      isToggleOn:!prevState.isToggleOn
+    }));
+    // this.setState({
+    //   isToggleOn:!this.state.isToggleOn
+    // });
+  }
+  render(){
+    return(
+      <button onClick={()=>{this.handleClick()}}>
+        {this.state.isToggleOn?'ON':'OFF'}
+      </button>
+    );
+  }
+}
+class Clock extends React.Component {
+  constructor( props){
+      super(props);
+      this.state={
+        date:new Date()
+      };
+  }
+  componentDidMount(){
+    clearInterval(this.timer);
+    this.timer=setInterval(()=>{
+         this.tick();
+    },1000)
 
+  }
+  componentWillUnmount(){
+    clearInterval(this.timer);
+  }
+  tick(){
+    this.setState({
+      date:new Date()
+    });
+  }
+  render(){
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+  
+}
+  root.render(<Toggle  />);
+
+// function Clock(props) {
+//   return (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {props.date.toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+// }
+// function tick() {
+  // root.render(<Clock  />);
+// }
+// setInterval(tick, 1000);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
